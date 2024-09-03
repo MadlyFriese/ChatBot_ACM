c3221c86383a8901131eba9ff2907871af5edf0e
MadlyFriese <sriharsha2k05@gmail.com>
2024-09-04 02:16:15 +0530

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,51 @@
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
+import streamlit as st
+
+
+st.title('Chatbot with multiple LLMs')
+
+if "chat_log" not in st.session_state:
+    st.session_state.chat_log = []
+
+if "previous_model" not in st.session_state:
+    st.session_state.previous_model = None
+
+option = st.selectbox(
+    "Choose the model",
+    ("llama2", "llama3.1:8b", "stable-code"),
+    index=0,
+    placeholder="Model",
+)
+
+if st.session_state.previous_model != option:
+    st.session_state.chat_log = []
+    st.session_state.previous_model = option
+
+llm = None
+
+if option == "llama2":
+    llm = Ollama(model="llama2")
+elif option == "llama3.1:8b":
+    llm = Ollama(model="llama3.1:8b")
+elif option == "stable-code":
+    llm = Ollama(model="stable-code")
+
+if llm:
+    input_text = st.text_input("Input your query")
+    
+    output_parser = StrOutputParser()
+
+    if input_text:
+        st.session_state.chat_log.append(f"User: {input_text}")
+
+        conversation_history_str = "\n".join(st.session_state.chat_log)
+        formatted_prompt = f"You are a chatbot. Please reply to the input given by the user accordingly.Remember what you were asked previously for context on future prompts\n\n{conversation_history_str}"
+
+        response = llm(formatted_prompt)
+        
+        parsed_response = output_parser.parse(response)
+        
+        st.session_state.conversation_history.append(f"Bot: {parsed_response}")
+
+        st.write(parsed_response)
